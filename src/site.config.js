ca5bfa64825921c35b7d7387ad50022febee515e
saweima12 <saweima12@gmail.com>
2022-05-02 18:35:00 +0800

    add more site setting.

--- src/site.config.js
+++ src/site.config.js
@@ -1,6 +1,15 @@
 const config = {
-  title: "Saweicore Stu.",
-  description: "",
+  title: "Saweicore Stu",
+  description: "幣圈新知 | 程式開發 | 網站架設 | 各項知識集散地",
+
+  text: {
+    more: "更多內容",
+  },
+
+  pagination: {
+    maxPerPage: 10
+  },
+
 	classifier: [
 		{ id: 'post', params: { path: '/_posts/' }, type: 'directory' },
 		{ id: 'tag', params: { keys: ['tag', 'tags'] }, type: 'frontmatter' }
