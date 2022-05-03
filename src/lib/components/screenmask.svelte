ae2410e9f04192ce912823f00b9e2170603ab259
saweima12 <saweima12@gmail.com>
2022-05-04 00:40:45 +0800

    mask starts to listening navgation

--- src/lib/components/screenmask.svelte
+++ src/lib/components/screenmask.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { isNavMenuShow, isMaskShow } from '$lib/store';
-    
+    import { afterNavigate, beforeNavigate } from '$app/navigation';
+
     let isVisible = false;
     isMaskShow.subscribe(value => isVisible = value);
 
@@ -8,6 +9,8 @@
         isNavMenuShow.set(false);
         setTimeout(()=> isMaskShow.set(false), 300);
     };
+
+    beforeNavigate(() => closeMaskHandle());
 </script>
 
 <div class="fixed top-0 z-10 w-screen h-screen screenmask" 
