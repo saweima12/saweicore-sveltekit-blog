19d3b1849ccb7f39d841792694dd54cc186c8b2e
saweima12 <saweima12@gmail.com>
2022-05-04 15:24:47 +0800

    adjust icon function

---
+++ src/icons/openbook.svelte
@@ -0,0 +1,53 @@
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+<g>
+	<g>
+		<path fill="currentColor" d="M464.495,60.701v-31.67H47.505v31.67H0v403.794c0,10.186,8.288,18.474,18.474,18.474h475.052
+			c10.186,0,18.474-8.288,18.474-18.474V60.701H464.495z M343.093,44.866h47.505v126.091l-15.835-9.237V95.01h-15.835v66.71
+			l-15.835,9.237V44.866z M263.918,44.866h63.34v153.661l39.588-23.093l39.588,23.093V44.866h42.227v279.753H263.918V44.866z
+			 M263.918,340.454H448.66v21.113H308.784v15.835H448.66v21.113H263.918V340.454z M63.34,44.866h184.742v279.753H63.34V44.866z
+			 M63.34,340.454h184.742v21.113H108.206v15.835h139.876v21.113H63.34V340.454z M496.165,464.495c0,1.43-1.209,2.639-2.639,2.639
+			H18.474c-1.43,0-2.639-1.209-2.639-2.639V76.536h31.67v248.083v7.917v81.814h416.99v-81.814v-7.917V76.536h31.67V464.495z"/>
+	</g>
+</g>
+<g>
+	<g>
+		<rect  fill="currentColor" x="92.371" y="71.258" width="31.67" height="15.835"/>
+	</g>
+</g>
+<g>
+	<g>
+		<rect  fill="currentColor" x="92.371" y="102.928" width="73.897" height="15.835"/>
+	</g>
+</g>
+<g>
+	<g>
+		<rect  fill="currentColor" x="92.371" y="145.155" width="126.68" height="15.835"/>
+	</g>
+</g>
+<g>
+	<g>
+		<rect  fill="currentColor" x="92.371" y="187.381" width="126.68" height="15.835"/>
+	</g>
+</g>
+<g>
+	<g>
+		<rect  fill="currentColor" x="92.371" y="229.608" width="126.68" height="15.835"/>
+	</g>
+</g>
+<g>
+	<g>
+		<rect  fill="currentColor" x="92.371" y="271.835" width="126.68" height="15.835"/>
+	</g>
+</g>
+<g>
+	<g>
+		<rect  fill="currentColor" x="292.948" y="229.608" width="126.68" height="15.835"/>
+	</g>
+</g>
+<g>
+	<g>
+		<rect  fill="currentColor" x="292.948" y="271.835" width="126.68" height="15.835"/>
+	</g>
+</g>
+</svg>
---
+++ src/lib/components/ticon.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { iconMap } from '$lib/store';
+
+    export let key: string;
+    export let hex: string = "";
+</script>
+
+<span style:color={hex || '#000'}>
+    {@html $iconMap[key]}
+</span>
