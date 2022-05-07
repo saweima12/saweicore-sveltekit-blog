1a2efe3145ffc15f51ea37cdb0bf5864df7c6057
saweima12 <saweima12@gmail.com>
2022-05-07 19:20:14 +0800

    add context supoort

--- src/config/nav.js
+++ src/config/nav.js
@@ -1,7 +1,7 @@
 const config = [
-	{ id: 'journal', name: '檔案庫', link: '/journal/' },
-	{ id: 'about', name: '關於本站', link: '/about/' },
-	{ id: 'github', name: 'Github', link: 'http://github.com' }
+	{ id: 'journal', name: '檔案庫', link: '/journal' },
+	{ id: 'about', name: '關於本站', link: '/about' },
+	{ id: 'github', name: 'Github', link: 'http://github.com/saweima12' }
 ];
 
 export default config;
--- src/config/textlang.js
+++ src/config/textlang.js
@@ -1,7 +1,14 @@
 const config = {
 	common: {
-		home: '首頁'
+		home: '首頁',
 	},
+  home: {
+    posts: '文章列表',
+    tags: '標籤清單',
+    tagName: '標籤名稱',
+    tagUpdated: '更新日期',
+    tagCount: '文章數'
+  },
 	journal: {
 		title: '檔案庫'
 	}
