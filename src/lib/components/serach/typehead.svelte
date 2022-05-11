ca391050b008a4eb3e9861a0ad52dee12c87c43e
saweima12 <saweima12@gmail.com>
2022-05-12 00:37:17 +0800

    add typehead list

---
+++ src/lib/components/serach/typehead.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let hits: Array<any>;
+</script>
+
+{#if hits}
+    <div class="hit-list">
+        {#each hits as hit}
+            <div class="hit-list-item">
+                
+
+            </div>
+        {/each}
+    </div>
+{/if}
---
+++ src/lib/components/widgets/authorwidget.svelte
@@ -0,0 +1,3 @@
+<div style="background:#bbb">
+    AuthorWidget
+</div>
