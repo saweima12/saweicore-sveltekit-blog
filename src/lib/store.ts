b7e24a1871549fbe216c7adcb2f40c9804cb628a
saweima12 <saweima12@gmail.com>
2022-05-15 22:49:09 +0800

    add theme support

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -2,6 +2,7 @@ import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
 
 export const siteConfig: Writable<Record<string, any>> = writable({});
+export const themeMode = writable("light");
 export const isMaskShow = writable(false);
 export const isNavMenuShow = writable(false);
 export const isSearBoxShow = writable(false);
