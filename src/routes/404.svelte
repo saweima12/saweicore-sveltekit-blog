ed8addc4954a7fe3d1b846b7207cc6d0703506dc
saweima12 <saweima12@gmail.com>
2022-05-17 16:50:38 +0800

    add 404 page

---
+++ src/routes/404.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import FzfIcon from "$lib/icons/fzf.svelte";
+</script>
+
+<div class="fzf-page">
+
+    <h1 class="mt-8 letter-title-font fzf-msg">
+        404 Not Found
+    </h1>
+
+    <div class="p-20 fzf-icon">
+        <FzfIcon />
+    </div>
+</div>
+
+
+<style>
+    .fzf-msg {
+        font-size: 2rem;
+        color: var(--text);
+    }
+
+    .fzf-icon {
+        color: var(--text);
+    }
+
+</style>
