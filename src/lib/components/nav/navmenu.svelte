ed6248fd40b0486a71266f2016bcfd359c33db80
saweima12 <saweima12@gmail.com>
2022-05-23 01:05:04 +0800

    add thememode switch to navmenu

--- src/lib/components/nav/navmenu.svelte
+++ src/lib/components/nav/navmenu.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { isExternal } from '$lib/client/helper';
 	import type { NavItem } from '$lib/types';
-	import { isNavMenuShow, siteConfig } from '$lib/store';
+	import { isNavMenuShow, isMaskShow, themeMode, siteConfig } from '$lib/store';
 	import NavIcon from '$lib/components/nav/navicon.svelte';
 	import ExternalIcon from '$lib/icons/external.svelte';
 
@@ -10,13 +10,22 @@
 	const navList: Array<NavItem> = $siteConfig.nav;
 	let isVisible = false;
 	isNavMenuShow.subscribe((value) => (isVisible = value));
+
+	const keyHandler = (e: KeyboardEvent) => {
+		if (e.key == "Escape" && isVisible) {
+			isMaskShow.set(false);
+			isNavMenuShow.set(false);
+		}
+	}
 </script>
+
+<svelte:window on:keydown={(e) => keyHandler(e)} />
 
 <div
 	class="fixed z-30 top-0 right-0 bottom-0 w-60 md:w-80 transition-all duration-200 navmenu"
 	class:translate-x-full={!isVisible}
 >
-	<div class="flex flex-col navmenu-container">
+	<div class="flex flex-col h-full navmenu-container">
 		<div class="mx-6 py-4 mb-6 author">
 			<div class="flex flex-row avatar-block">
 				<img src={author.avatar} alt="saweima" />
@@ -50,6 +59,18 @@
 				</li>
 			{/each}
 		</ul>
+		<div class="grow spacer"></div>
+		<div class="flex items-center mb-8 pl-8 nav-item">
+			<div class="icon-base w-6">
+				<NavIcon key="theme" />
+			</div>
+			<a href="#!" 
+			   class="pl-4 text-xl letter-content-font" 
+			   on:click={() => themeMode.set($themeMode == "light" ? "dark" : "light")}>
+				{textlang.themeMode}
+			</a>
+			<div class="icon-base w-3" />
+		</div>
 	</div>
 </div>
 
