3abf1f00d9caff0e57176deb6ef86cfad667af2b
saweima12 <saweima12@gmail.com>
2022-05-23 18:16:24 +0800

    add about page content

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -9,11 +9,40 @@
 		const { metadata, content }: PageResult = await response.json();
 
 		return {
-			props: {}
+			props: {
+				metadata,
+				content
+			}
 		};
 	};
 </script>
 
 <script lang="ts">
+import { siteConfig } from '$lib/store';
+import { getTitleStr } from '$lib/client';
 
+export let content: string;
+export let metadata: Record<string,any>;
 </script>
+
+<svelte:head>
+	<title>{metadata.title} | {getTitleStr($siteConfig)}</title>
+</svelte:head>
+
+<div class="my-10 about-page wrapper">
+	<header class="mb-4 mx-6">
+		<h1 class="font-bold letter-title-font post-title">
+			{metadata.title}
+		</h1>
+	</header>
+	<div class="content">
+		{@html content}
+	</div>
+</div>
+
+<style>
+	.post-title {
+		font-size: 32px;
+	}
+
+</style>
