5807bd8032a01a06d7187c60e1e2941fdcf1807e
saweima12 <saweima12@gmail.com>
2022-05-25 13:58:16 +0800

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@ set -e
 
 pnpm run build
 pnpm run postbuild
-GIT_URL="git@github.com:saweima12/saweima12.github.io.git master"
+GIT_URL="git@github.com:saweima12/saweima12.github.io.git"
 BUILD_DATE=$(date +"%Y%m%d%H%M")
 
 # go into build directory
@@ -15,8 +15,8 @@ touch .nojekyll
 
 # git process
 git init 
-git add -A
+git add -Af
 git commit -m "deploy at $BUILD_DATE"
 
-
-git-ssh -i ~/.ssh/cctips push -f $GIT_URL
+# commit
+git-ssh -i ~/.ssh/cctips push -f $GIT_URL master
