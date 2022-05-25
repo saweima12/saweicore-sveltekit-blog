27aaed25ce44fb98af8b203ce99ffe7bc47f00b1
saweima12 <saweima12@gmail.com>
2022-05-25 14:21:43 +0800

    add totop icon

---
+++ src/lib/icons/totop.svelte
@@ -0,0 +1,8 @@
+<svg class="go-to-top" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 49.484 28.284">
+    <rect fill="currentColor" width="40" height="3" rx="2" transform="translate(4.5)"/>
+    <g transform="translate(-229 -126.358)" fill="currentColor">
+      <rect width="35" height="5" rx="2" transform="rotate(-45 296.902 -200.874)"/>
+      <rect width="35" height="5" rx="2" transform="rotate(-135 169.502 20.377)"/>
+    </g>
+</svg>
+  
