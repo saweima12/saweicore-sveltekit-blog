e50958dc2e5579551f6790e9f29446016f32d262
saweima12 <saweima12@gmail.com>
2022-05-25 13:58:09 +0800

    add type mark to store.ts

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -2,8 +2,8 @@ import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
 
 export const siteConfig: Writable<Record<string, any>> = writable({});
-export const themeMode = writable("light");
-export const isMaskShow = writable(false);
-export const isNavMenuShow = writable(false);
-export const isSearBoxShow = writable(false);
-export const lightBoxContent = writable("");
+export const themeMode: Writable<string> = writable("light");
+export const isMaskShow: Writable<boolean> = writable(false);
+export const isNavMenuShow: Writable<boolean> = writable(false);
+export const isSearBoxShow: Writable<boolean> = writable(false);
+export const lightBoxContent: Writable<string> = writable("");
