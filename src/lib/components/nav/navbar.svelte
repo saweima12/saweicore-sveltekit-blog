7a99e2006e00bf9879309adfecd43169ca35db6b
saweima12 <saweima12@gmail.com>
2022-05-28 21:23:25 +0800

    add auto hide navbar on scroll down

--- src/lib/components/nav/navbar.svelte
+++ src/lib/components/nav/navbar.svelte
@@ -1,11 +1,23 @@
-<script>
+<script lang="ts">
 	import { siteConfig } from '$lib/store';
 	import Hamburger from '$lib/components/nav/hamburger.svelte';
 	import SearchWidget from '$lib/components/widgets/searchwidget.svelte';
 
+	let prevScrollY: number = 0;
+	let newScrollY: number = 0;
+	let isHide: boolean = false;
+	$: { 
+		if (Math.abs(newScrollY - prevScrollY) > 80) {
+			isHide = newScrollY - prevScrollY > 0 
+			prevScrollY = newScrollY;
+		}
+	};
 </script>
 
-<nav class="flex items-center h-14 p-2 top-navbar">
+<svelte:window bind:scrollY={newScrollY}/>
+
+<nav class="flex items-center h-14 p-2 top-navbar"
+	class:hide={isHide}>
 	<a class="flex ml-2 raleway-font site-title" href="/">
 		<div class="logo">
 			<img src="/logo.png" alt={$siteConfig.title} />
@@ -24,10 +36,21 @@
 
 <style>
 
+.top-navbar {
+	translate: all .3s;
+}
+
+.top-navbar.hide {
+	transform: translateY(-60px);
+}
+
 .top-navbar .logo {
 	width: 35px;
 	height: 35px;
+
 }
+
+
 
 .top-navbar .search-box{
 	width: 10rem;
