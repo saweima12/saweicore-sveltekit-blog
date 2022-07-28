455d3cee850cc86df13df29944ba766c116c3d42
saweima12 <saweima12@gmail.com>
2022-07-28 13:38:50 +0800

    update: package version & author config

--- src/site.config.js
+++ src/site.config.js
@@ -10,23 +10,24 @@ const config = {
 	description: 'SaweiCore Lab 是用於記錄與歸檔個人的研究心得的集散地，內容多以網站架設、程式開發、幣圈探索為主並以需求導向探討問題並記錄對應的解決方案，以便後續查閱。',
 	url: "https://saweicore.com",
 	ga: "G-KY269QECHX",
-		author: {
-			name: 'Saweima',
-		  summary: '邊緣工作者',
-			description: '雜項工程師 | 幣圈小白 | 想到再寫',
-			avatar: 'https://avatars.githubusercontent.com/u/62002212'
-		},
-		pagination: {
-			maxPerPage: 8
-		},
-		classifier: [
-			{ id: 'post', params: { path: '/_posts/' }, type: 'directory' },
-			{ id: 'tag', params: { keys: ['tag', 'tags'] }, type: 'frontmatter' },
-		],
-		marked: {
-			options: {},
-			extensions: [headingClassName("heading-item"), customALink()]
-		},
+  author: {
+    name: 'Saweima',
+    email: 'saweima12@gmail.com',
+    summary: '邊緣工作者',
+    description: '雜項工程師 | 幣圈小白 | 想到再寫',
+    avatar: 'https://avatars.githubusercontent.com/u/62002212'
+  },
+  pagination: {
+    maxPerPage: 8
+  },
+  classifier: [
+    { id: 'post', params: { path: '/_posts/' }, type: 'directory' },
+    { id: 'tag', params: { keys: ['tag', 'tags'] }, type: 'frontmatter' },
+  ],
+  marked: {
+    options: {},
+    extensions: [headingClassName("heading-item"), customALink()]
+  },
 	search: {
 		appKey: "BHODY4NQKK",
 		apiKey: "5f4c05344cf6d1cbe581c20e049a7c14",
