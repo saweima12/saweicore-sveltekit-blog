4797c82cdfe6b952268ebbd2141e59ab93f3cc21
saweima12 <saweima12@gmail.com>
2022-08-07 14:09:01 +0800

    add error template

---
+++ src/routes/__error.svelte
@@ -0,0 +1,52 @@
+<script lang="ts" context="module">
+    import type { Load } from '@sveltejs/kit';
+	export const load: Load = ({error, status}) => {
+		return {
+			props: { error, status }
+		};
+	}
+</script>
+
+<script lang="ts">
+	import { dev } from '$app/env';
+
+	export let error: Error;
+    export let status: string;
+</script>
+
+<svelte:head>
+	<title>{status}</title>
+</svelte:head>
+
+<div class="col-md-9">
+	<h1>{status}</h1>
+
+	<p>{error.message}</p>
+
+	{#if dev && error.stack}
+		<pre>{error.stack}</pre>
+	{/if}
+</div>
+
+<style>
+	h1,
+	p {
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
