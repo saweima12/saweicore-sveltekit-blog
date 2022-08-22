d89431c544da509d0614a0efd077c98bb16efdf3
saweima12 <saweima12@gmail.com>
2022-08-22 12:03:08 +0800

    use a tag wrap up postlistitem

--- src/lib/components/article/postlistitem.svelte
+++ src/lib/components/article/postlistitem.svelte
@@ -10,73 +10,69 @@
 </script>
 
 <section class="mx-6 mt-5 post-item">
-	<header class="mb-3 post-header">
-		<div class="flex items-center">
-			<div class="icon-base w-4 h-4"><CalenderIcon /></div>
-			<time class="flex self-center text-sm ml-1 created-date">{getYYYYMMDD(metadata.created)}</time>
-		</div>
-	</header>
-
-	<main class="flex flex-row mb-2 post-content">
-		<div class="flex-1 context">
-			<div class="letter-title-font font-bold ">
-				<a sveltekit:prefetch class="post-title" href={pageRoute.getPostPath(post)} alt="{metadata.title}">
-					{metadata.title}
-				</a>
+	<a sveltekit:prefetch  href={pageRoute.getPostPath(post)} alt="{metadata.title}">
+		<header class="mb-3 post-header">
+			<div class="flex items-center">
+				<div class="icon-base w-4 h-4"><CalenderIcon /></div>
+				<time class="flex self-center text-sm ml-1 created-date">{getYYYYMMDD(metadata.created)}</time>
 			</div>
+		</header>
 
-			<div class="hidden sm:inline-block summary">
-				{#if metadata.excerpt}
-				<a sveltekit:prefetch href={pageRoute.getPostPath(post)} alt="{metadata.title}">
+		<main class="flex flex-row mb-2 post-content">
+			<div class="flex-1 context">
+				<div class="letter-title-font font-bold ">
+					<h2 class="post-title">
+						{metadata.title}
+
+					</h2>
+				</div>
+
+				<div class="hidden sm:inline-block summary">
+					{#if metadata.excerpt}
 						<p class="letter-content-font">
 							{metadata.excerpt.length > 100
 								? metadata.excerpt.slice(0, 100) + '...'
 								: metadata.excerpt}
 						</p>
-					</a>
-				{/if}
-			</div>
-
-			<footer class="flex items-center my-3">
-				<div class="flex items-center">
-					<div class="flex post-tag-list">
-						{#if tags}
-							<div class="flex self-center mr-2 post-tag-item">
-								<a sveltekit:prefetch href={pageRoute.getTagPath(tags[0])}>{tags[0]}</a>
-							</div>
-							{#each tags.slice(1, 5) as tag}
-								<div class="hidden sm:flex self-center post-tag-item">
-									<a sveltekit:prefetch href={pageRoute.getTagPath(tag)}>{tag}</a>
-								</div>
-							{/each}
-						{/if}
-					</div>
+					{/if}
 				</div>
-				<div class="grow spacer" />
 
-				{#if !metadata.thumbnail}
-					<div class="mr-2 flex self-center more">
-						<a sveltekit:prefetch href={pageRoute.getPostPath(post)} alt="{metadata.title}">
+				<footer class="flex items-center my-3">
+					<div class="flex items-center">
+						<div class="flex post-tag-list">
+							{#if tags}
+								<div class="flex self-center mr-2 post-tag-item">
+									<a sveltekit:prefetch href={pageRoute.getTagPath(tags[0])}>{tags[0]}</a>
+								</div>
+								{#each tags.slice(1, 5) as tag}
+									<div class="hidden sm:flex self-center post-tag-item">
+										<a sveltekit:prefetch href={pageRoute.getTagPath(tag)}>{tag}</a>
+									</div>
+								{/each}
+							{/if}
+						</div>
+					</div>
+					<div class="grow spacer" />
+
+					{#if !metadata.thumbnail}
+						<div class="mr-2 flex self-center more">
 							<div class="icon-base w-6 h-6">
 								<MoreIcon />
 							</div>
-							<p class="hidden">{metadata.title}</p>
-						</a>
-					</div>
-				{/if}
-			</footer>
-		</div>
-		{#if metadata.thumbnail}
-			<div class="min-w-20 ml-3 align-center thumbnail">
-				<a href={pageRoute.getPostPath(post)} alt="{metadata.title}">
+						</div>
+					{/if}
+				</footer>
+			</div>
+			{#if metadata.thumbnail}
+				<div class="min-w-20 ml-3 align-center thumbnail">
 					<img
 						src={metadata.thumbnail}
 						class="object-cover thumbnail-pic"
 						alt="{metadata.title}"
 					/>
 					<p class="hidden">{metadata.title}</p>
-				</a>
-			</div>
-		{/if}
-	</main>
+				</div>
+			{/if}
+		</main>
+	</a>
 </section>
