57a4707ea321ea5d64ec0f6ac8e8824d62f64c25
saweima12 <saweima12@gmail.com>
2022-08-23 00:12:05 +0800

    add comment component

---
+++ src/lib/components/comment.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { siteConfig, themeMode } from '$lib/store';
+    import Utterances from '$lib/vendor/utterances.svelte';
+
+	const comment: Record<string, any> = $siteConfig.utterances;
+	// handle comment-theme
+	$: theme = $themeMode == "light" ? comment.light : comment.dark;
+
+</script>
+
+<Utterances reponame="{$siteConfig.utterances.repo}" issueTerm="og:title" {theme} />
