cc8033be4c04115da9cb49b3590528f4ef8a003f
saweima12 <saweima12@gmail.com>
2022-09-06 13:13:50 +0800

    remove unused endpoint

--- src/routes/api/config.json/+server.ts
+++
@@ -1,8 +0,0 @@
-import { json } from '@sveltejs/kit';
-import type { RequestHandler } from '@sveltejs/kit';
-import { siteConfig } from 'markedpage';
-
-export const GET: RequestHandler = async ({ url }) => {
-	const config = await siteConfig();
-	return json(config);
-};
--- src/routes/api/test/search.json/+server.ts
+++
@@ -1,9 +0,0 @@
-import { json as json$1 } from '@sveltejs/kit';
-import type { RequestHandler } from '@sveltejs/kit';
-import TestJson from '$lib/test/search.json';
-
-export const GET: RequestHandler = async ({ url }) => {
-	return json$1({
-		...TestJson
-	});
-};
