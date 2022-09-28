7c1009b81f534201eec13148cc5d000c018e8584
saweima12 <saweima12@gmail.com>
2022-09-28 13:36:26 +0800

    remove implement

--- src/lib/components/article/infinitescroll.svelte
+++ src/lib/components/article/infinitescroll.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
-	export let throttle = 250;
-	export let offset = 0;
 </script>
 
 <div class="mx-5 more-btn">
-	<button class="py-2 border-2 box-border w-full" on:click={clickHandler} bind:this={component}>
-		More..</button
-	>
+
 </div>
